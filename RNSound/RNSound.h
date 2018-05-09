4b853c148b1af2488c5402385a7812f2afa3d780
Riku Raisanen <rikurr@gmail.com>
2018-05-09 17:49:40 -0400

    tryfix RctBridgeModule.h error

--- RNSound/RNSound.h
+++ RNSound/RNSound.h
@@ -1,7 +1,7 @@
 #if __has_include(<React/RCTBridgeModule.h>)
     #import <React/RCTBridgeModule.h>
 #else
-    #import "RCTBridgeModule.h"
+    #import "React/RCTBridgeModule.h"
 #endif
 
 #import <AVFoundation/AVFoundation.h>
